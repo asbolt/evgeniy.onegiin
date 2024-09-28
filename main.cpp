ada4b050549fea3a758793ed991a6701ee4599f9
asbolt <asboltova@gmail.com>
2024-09-29 00:07:31 +0300

    qsort

--- main.cpp
+++ main.cpp
@@ -9,67 +9,98 @@
 enum Status 
 {
     SUCCESS = 0, 
-    ERROR = 1
+    ERROR = 1,
+    SECONDSUCCESS = 2
 };
 
-Status readFile (char **buffer, int *strings, long int *size);
-Status giveRealAdresses (char *adress[], char *buffer, long int size, int strings);
-Status sortStrings (int strings, char *adress[]);
-Status compare (char firstSumbol, char secondSumbol);
-Status printSortText (char *adress[], int strings);
-Status RABOTAUPJPJPJ (char *adress[], int stringNumber);
-Status sortToFirstCondition (int strings, char *adress[]);
-
-
-int main ()
+struct Text
 {
     char *buffer = NULL;
     int strings = 0;
     long int size = 0;
+};
 
-    readFile (&buffer, &strings, &size);
 
-    char **adress= (char**)calloc(strings, sizeof(char*));
-    assert (*adress == NULL);
 
-    giveRealAdresses (adress, buffer, size, strings);
+typedef Status(*CompareFunction)(const void *firstSumbol, const void *secondSumbol);
+typedef Status(*ExchangeFunction)(void *adressStrings[], int stringNumber);
 
-    sortStrings (strings, adress);
+Status compareStrings(const void *firstSumbol, const void *secondSumbol);
+Status exchangeStrings (void *adressStrings[], int stringNumber);
+Status readFile (Text *text);
+Status removeNewLines (char *adressStrings[], Text text);
+Status printSortText (char *adressStrings[], int strings);
+Status sortToFirstCondition (int strings, char *adressStrings[]);
+Status initStringsArray (int strings, char ***adressStrings);
+void mySort (void *adressStrings[], CompareFunction compareFunction, int strings, ExchangeFunction exchangeFunction);
 
-    printSortText (adress, strings);
+int main ()
+{
+    Text text;
 
-    sortToFirstCondition (strings, adress);
+    if (readFile (&text) == ERROR)
+    {
+        return ERROR;
+    }
+ 
+    char **adressStrings = NULL;
+    initStringsArray (text.strings, &adressStrings);
 
+    removeNewLines (adressStrings, text);
+
+    mySort ((void**)adressStrings, compareStrings, text.strings, exchangeStrings);
+
+    printSortText (adressStrings, text.strings);
+
+    sortToFirstCondition (text.strings, adressStrings);
+
+    printf ("\n \n \n");
     printf ("Изначальный вариант текста:\n");
-    printSortText (adress, strings);
+    printSortText (adressStrings, text.strings);
 }
 
-Status readFile (char **buffer, int *strings, long int *size)
+Status initStringsArray (int strings, char ***adressStrings)
 {
+    *adressStrings = (char**)calloc(strings, sizeof(char*));
+    if (*adressStrings == NULL)
+    {
+        return ERROR;
+    }
 
+    return SUCCESS;
+}
+
+Status readFile (Text *text)
+{
     FILE * ptrFile = fopen ("text.txt", "rb");
-    //assert (ptrFile == NULL);
+    if (ptrFile == NULL)
+    {
+        return ERROR;
+    }
 
     fseek (ptrFile, 0, SEEK_END);
-    *size = ftell (ptrFile);
+    text -> size = ftell (ptrFile);
     fseek (ptrFile, 0, SEEK_SET);
 
-    *buffer = (char*)calloc (*size+1, sizeof(char));
-    //assert (buffer == NULL);
-
-    fread (*buffer, sizeof(char), *size, ptrFile);
-    (*buffer) [*size] = '\n';
-
-     for (int number = 0; number < *size; number ++)
+    text -> buffer = (char*)calloc (text -> size +1, sizeof(char));
+    if (text -> buffer == NULL)
     {
-        if ((*buffer)[number] == '\n')
+        return ERROR;
+    }
+
+    fread (text -> buffer, sizeof(char), text -> size, ptrFile);
+    (text -> buffer) [text -> size] = '\n';
+
+    for (int number = 0; number < text -> size; number ++)
+    {
+        if ((text -> buffer)[number] == '\n')
         {
-            (*strings)++;
+            (text -> strings)++;
         }
 
-        if ((*buffer)[number] == '\r')
+        if ((text -> buffer)[number] == '\r')
         {
-            (*buffer)[number] = '\n';
+            (text -> buffer)[number] = '\n';
         }
     }
 
@@ -78,22 +109,22 @@ Status readFile (char **buffer, int *strings, long int *size)
     return SUCCESS;
 }
 
-Status giveRealAdresses (char *adress[], char *buffer, long int size, int strings)
+Status removeNewLines (char *adressStrings[], Text text)
 {
-    adress[0] = buffer;
+    adressStrings[0] = text.buffer;
 
     int numberString = 1;
 
-    for (int numberSumbol = 0; numberSumbol < size; numberSumbol++)
+    for (int numberSumbol = 0; numberSumbol < text.size; numberSumbol++)
     {
-        if (buffer[numberSumbol] == '\n')
+        if (text.buffer[numberSumbol] == '\n')
         {
-            adress[numberString] = &buffer[numberSumbol+2];
+            adressStrings[numberString] = &text.buffer[numberSumbol+2];
 
             numberSumbol += 2;
             numberString++;
 
-            if (numberString == strings)
+            if (numberString == text.strings)
             {
                 return SUCCESS;
             }
@@ -103,58 +134,39 @@ Status giveRealAdresses (char *adress[], char *buffer, long int size, int string
     return SUCCESS;
 }
 
-Status compare (char firstSumbol, char secondSumbol)
+Status compareStrings (const void *firstSumbol, const void *secondSumbol)
 {
-    if (((isalpha (firstSumbol) != 0 && isalpha (secondSumbol) != 0 &&
-        toupper(firstSumbol) > toupper(secondSumbol)) || isalpha (secondSumbol) == 0) ||
-        firstSumbol == '\n' || secondSumbol == '\n')
+    if (((isalpha (*(const char*)firstSumbol) != 0 && isalpha (*(const char*)secondSumbol) != 0 &&
+        toupper(*(const char*)firstSumbol) > toupper(*(const char*)secondSumbol)) || isalpha (*(const char*)secondSumbol) == 0) ||
+        *(const char*)firstSumbol == '\n' || *(const char*)secondSumbol == '\n')
     {
         return SUCCESS;
+    } else if (((isalpha (*(const char*)firstSumbol) != 0 && isalpha (*(const char*)secondSumbol) != 0 &&
+        toupper(*(const char*)firstSumbol) < toupper(*(const char*)secondSumbol)) || isalpha (*(const char*)secondSumbol) == 0) ||
+        *(const char*)firstSumbol == '\n' || *(const char*)secondSumbol == '\n')
+    {
+        return SECONDSUCCESS;
     }
 
     return ERROR;
 }
 
-Status RABOTAUPJPJPJ (char *adress[], int stringNumber)
+Status exchangeStrings (void *adressStrings[], int stringNumber)
 {
-   for (int sumbolNumber = 0; 
-         compare(*(adress[stringNumber] + sumbolNumber), *(adress[stringNumber+1] + sumbolNumber)) == SUCCESS; 
-         sumbolNumber++)
-    {   
-        if (compare (*(adress[stringNumber] + sumbolNumber), *(adress[stringNumber + 1] + sumbolNumber)) == SUCCESS)
-        {
-            char *variableForCopy = adress[stringNumber];
-            adress[stringNumber] = adress[stringNumber + 1];
-            adress[stringNumber+1] = variableForCopy;
-
-            return SUCCESS;
-        } 
-    }
+    char *variableForCopy = *((char**)adressStrings + stringNumber);
+    *((char**)adressStrings + stringNumber) = *((char**)adressStrings + stringNumber + 1);
+    *((char**)adressStrings + stringNumber + 1) = variableForCopy;
 
     return SUCCESS;
 }
 
-Status sortStrings (int strings, char *adress[])
-{
-
-    for (int numberSort = 0; numberSort < strings - 1; numberSort++)
-    {
-        for (int stringNumber = 0; stringNumber < strings - 1; stringNumber++)
-        {
-            RABOTAUPJPJPJ (adress, stringNumber);
-        }
-    }
-
-    return SUCCESS;
-}
-
-Status printSortText(char *adress[], int strings)
+Status printSortText(char *adressStrings[], int strings)
 {
     for (int string = 0; string < strings; string++)
     {
-        for (int number = 0; *(adress[string] + number) != '\n'; number++)
+        for (int number = 0; *(adressStrings[string] + number) != '\n'; number++)
         {
-            printf ("%c", *(adress[string] + number));
+            printf ("%c", *(adressStrings[string] + number));
         }
 
         printf ("\n");
@@ -163,17 +175,17 @@ Status printSortText(char *adress[], int strings)
     return SUCCESS;
 }
 
-Status sortToFirstCondition (int strings, char *adress[])
+Status sortToFirstCondition (int strings, char *adressStrings[])
 {
     for (int numberSort = 0; numberSort < strings - 1; numberSort++)
     {
         for (int stringNumber = 0; stringNumber < strings - 1; stringNumber++)
         {
-            if (adress[stringNumber] > adress[stringNumber + 1])
+            if (adressStrings[stringNumber] > adressStrings[stringNumber + 1])
             {
-                char *variableForCopy = adress[stringNumber];
-                adress[stringNumber] = adress[stringNumber + 1];
-                adress[stringNumber+1] = variableForCopy;
+                char *variableForCopy = adressStrings[stringNumber];
+                adressStrings[stringNumber] = adressStrings[stringNumber + 1];
+                adressStrings[stringNumber+1] = variableForCopy;
             }
         }
     }
@@ -181,3 +193,29 @@ Status sortToFirstCondition (int strings, char *adress[])
     return SUCCESS;
 }
 
+void mySort (void *adressStrings[], CompareFunction compareFunction, int strings, ExchangeFunction exchangeFunction)
+{
+    for (int numberSort = 0; numberSort < strings - 1; numberSort++)
+    {
+        for (int stringNumber = 0; stringNumber < strings - 1; stringNumber++)
+        {
+            for (int sumbolNumber = 0, end = 0; 
+                 *((char*)(*((char**)adressStrings + stringNumber) + sumbolNumber)) != '\0' && 
+                 *((char*)(*((char**)adressStrings + stringNumber+1) + sumbolNumber)) != '\0' && 
+                 end != 1; 
+                 sumbolNumber++)
+            {   
+                if (compareFunction ((const void*)(((char*)(*((char**)adressStrings + stringNumber) + sumbolNumber))), 
+                    (const void*)(((char*)(*((char**)adressStrings + stringNumber + 1) + sumbolNumber)))) == SUCCESS)
+                {
+                    exchangeFunction (adressStrings, stringNumber);
+                    end = 1;
+                } else if (compareFunction ((const void*)(((char*)(*((char**)adressStrings + stringNumber) + sumbolNumber))), 
+                    (const void*)(((char*)(*((char**)adressStrings + stringNumber + 1) + sumbolNumber)))) == SECONDSUCCESS)
+                {
+                    end = 1;
+                }
+            }
+        }
+    }
+}
